858af1fdaddf8ef8010b5d7c4ba07fe642828268
lunghr <myilanka8@gmail.com>
2024-04-16 15:40:08 +0300

    init

---
+++ src/main/java/com/lunghr/count_math_lab2/CountMathLab2Application.java
@@ -0,0 +1,13 @@
+package com.lunghr.count_math_lab2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CountMathLab2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CountMathLab2Application.class, args);
+    }
+
+}
